fe0dae11787407b5919fb857c3c083d8d99c9a1f
Angelo Evangelista <angeloevan.ane@gmail.com>
2021-01-12 16:58:54 -0300

    Shared: UpdatedAt setter

--- OrderingSystem.Shared/Entities/Entity.cs
+++ OrderingSystem.Shared/Entities/Entity.cs
@@ -1,11 +1,38 @@
 using System;
+using Flunt.Notifications;
 
 namespace OrderingSystem.Shared.Entities
 {
-  public abstract class Entity
+  public abstract class Entity: Notifiable
   {
+    public Entity()
+    {
+      Id = new Guid();
+      Active = true;
+      CreatedAt = DateTime.Now;
+      UpdatedAt = DateTime.Now;
+    }
+
+    public Guid Id { get; private set; }
     public bool Active { get; private set; }
     public DateTime CreatedAt { get; private set; }
     public DateTime UpdatedAt { get; private set; }
+
+    protected void SetUpdatedAt() => UpdatedAt = DateTime.Now;
+
+    public virtual T Activate<T>() where T : Entity
+    {
+      SetUpdatedAt();
+      Active = true;
+
+      return this as T;
+    }
+    public virtual T Deactivate<T>() where T : Entity
+    {
+      SetUpdatedAt();
+      Active = false;
+
+      return this as T;
+    }
   }
 }
