8db5b3a46e03a5e019fb5182da46425f9a387dba
Angelo Evangelista <angeloevan.ane@gmail.com>
2021-01-13 21:54:08 -0300

    Data: Implementing ClientRepository

--- OrderingSystem.Data/Repositories/ClientRepository.cs
+++ OrderingSystem.Data/Repositories/ClientRepository.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using OrderingSystem.Data.Context;
@@ -16,14 +18,71 @@ namespace OrderingSystem.Data.Repositories
       _context = context;
     }
 
+    public async Task Delete(string clientId)
+    {
+      Guid clientGuid = new Guid(clientId);
+
+      var client = await _context.Clients.FindAsync(clientGuid);
+
+      client.Deactivate<Client>();
+
+      await _context.SaveChangesAsync();
+    }
+
+    public async Task<Client> Find(string clientId)
+    {
+      Guid clientGuid = new Guid(clientId);
+
+      var client = await _context.Clients
+        .AsNoTracking()
+        .Include(pre => pre.Orders)
+        .FirstOrDefaultAsync(pre =>
+          pre.Id.Equals(clientGuid)
+          && pre.Active
+        );
+
+      return client;
+    }
+
+    public async Task<Client> FindByEmail(string email)
+    {
+      var client = await _context.Clients
+        .AsNoTracking()
+        .Include(pre => pre.Orders)
+        .FirstOrDefaultAsync(pre =>
+          pre.Email.Equals(email)
+          && pre.Active
+        );
+
+      return client;
+    }
+
     public async Task<IEnumerable<Client>> List()
     {
       var clients = await _context.Clients
-        .Include(pre => pre.Orders)
         .AsNoTracking()
+        .Where(pre => pre.Active)
+        .Include(pre => pre.Orders)
         .ToListAsync();
 
       return clients;
+    }
+
+    public async Task<Client> Save(Client client)
+    {
+      await _context.Set<Client>().AddAsync(client);
+      await _context.SaveChangesAsync();
+
+      return client;
+    }
+
+    public async Task<Client> Update(Client client)
+    {
+      _context.Entry<Client>(client).State = EntityState.Modified;
+
+      await _context.SaveChangesAsync();
+
+      return client;
     }
   }
 }
